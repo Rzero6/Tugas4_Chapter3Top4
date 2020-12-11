cf6afd951171c5c9b08f703ba2c72777c293f4be
Reynolds <michaelreynoldk@gmail.com>
2020-12-11 22:43:02 +0100

    Version 1.1

---
+++ src/Main.kt
@@ -0,0 +1,49 @@
+fun main() {
+    print("Uang Tabungan(Euro): ")
+    var tabungan : Int = readLine()?.toInt()!!
+
+
+
+    print("\nIngin beli PS5 (true/false)? ")
+    val ps5 : Boolean = readLine()?.toBoolean()!!
+    if(ps5==true) {
+
+        tabungan -= 499
+
+    } else {
+
+    }
+    if(tabungan<0) {
+
+        tabungan += 499
+        println("\nUang tabungan tidak cukup untuk membeli PS5.")
+        println("Perlu ${499-tabungan} Euro untuk membeli PS5.")
+
+    } else {
+
+    }
+    print("\nIngin beli iPhone 12 (true/false)? ")
+    val iphone : Boolean = readLine()?.toBoolean()!!
+    if(iphone==true) {
+
+        tabungan -= 1499
+
+    } else {
+
+    }
+
+    if(tabungan<0) {
+
+        tabungan += 1499
+        println("\nUang tabungan tidak cukup untuk membeli iPhone 12.")
+        println("Perlu ${1499-tabungan} Euro untuk membeli iPhone 12.")
+
+    } else {
+
+    }
+
+
+
+    println("\nSisa tabungan: $tabungan")
+
+}
